d58ad889e2b109b76bf045071d63ea067a512854
patman <27874300+patmanf@users.noreply.github.com>
2024-08-20 02:11:06 -0500

    make quickbar.lua less terrible

---
+++ sys/stardust/quickbar/actions.lua
@@ -0,0 +1,41 @@
+local actions = {}
+qbActions = actions -- alias for execs
+
+function actions.pane(cfg)
+  if type(cfg) ~= "table" then cfg = { config = cfg } end
+  player.interact(cfg.type or "ScriptPane", cfg.config)
+end
+
+function actions.ui(cfg, data) -- metaGUI windows
+  player.interact("ScriptPane", { gui = { }, scripts = {"/metagui.lua"}, config = cfg, data = data })
+end
+
+function actions.exec(script, ...)
+  if type(script) ~= "string" then return nil end
+  params = {...} -- pass any given parameters to the script
+  _SBLOADED[script] = nil require(script) -- force execute every time
+  params = nil -- clear afterwards for cleanliness
+end
+
+function actions._legacy_module(s)
+  local m, e = (function() local it = string.gmatch(s, "[^:]+") return it(), it() end)()
+  local mf = string.format("/quickbar/%s.lua", m)
+  module = { }
+  _SBLOADED[mf] = nil require(mf) -- force execute
+  module[e]() module = nil -- run function and clean up
+end
+
+local function nullfunc() end
+
+function action(id, ...)
+  return (actions[id] or nullfunc)(...)
+end
+
+function legacyAction(i)
+  if i.pane then return { "pane", i.pane } end
+  if i.scriptAction then
+    sb.logInfo(string.format("Quickbar item \"%s\": scriptAction is deprecated, please use new entry format", i.label))
+    return { "_legacy_module", i.scriptAction }
+  end
+  return { "null" }
+end
--- sys/stardust/quickbar/quickbar.lua
+++ sys/stardust/quickbar/quickbar.lua
@@ -1,60 +1,7 @@
-require "/scripts/util.lua"
 require "/sys/quickbar/conditions.lua"
+require "/sys/stardust/quickbar/actions.lua"
 
-local actions = { }
-qbActions = actions -- alias in global for execs
-compactLabels = {}
-local function nullfunc() end
-local function action(id, ...) return (actions[id] or nullfunc)(...) end
-
--------------
--- actions --
--------------
-
-function actions.pane(cfg)
-  if type(cfg) ~= "table" then cfg = { config = cfg } end
-  player.interact(cfg.type or "ScriptPane", cfg.config)
-end
-
-function actions.ui(cfg, data) -- metaGUI windows
-  player.interact("ScriptPane", { gui = { }, scripts = {"/metagui.lua"}, config = cfg, data = data })
-end
-
-function actions.exec(script, ...)
-  if type(script) ~= "string" then return nil end
-  params = {...} -- pass any given parameters to the script
-  _SBLOADED[script] = nil require(script) -- force execute every time
-  params = nil -- clear afterwards for cleanliness
-end
-
-function actions._legacy_module(s)
-  local m, e = (function() local it = string.gmatch(s, "[^:]+") return it(), it() end)()
-  local mf = string.format("/quickbar/%s.lua", m)
-  module = { }
-  _SBLOADED[mf] = nil require(mf) -- force execute
-  module[e]() module = nil -- run function and clean up
-end
-
----------------
--- internals --
----------------
-
-local colorSub = { -- color tag substitutions
-  ["^essential;"] = "^#ffb133;",
-  ["^admin;"] = "^#bf7fff;",
-  ["^pat_stardust;"] = "^#C6C4FF;",
-}
-
-local function legacyAction(i)
-  if i.pane then return { "pane", i.pane } end
-  if i.scriptAction then
-    sb.logInfo(string.format("Quickbar item \"%s\": scriptAction is deprecated, please use new entry format", i.label))
-    return { "_legacy_module", i.scriptAction }
-  end
-  return { "null" }
-end
-
-local function metaguiSettings(key, default)
+local function getMetaguiSetting(key, default)
   local settings = player.getProperty("metagui:settings", {})
   if settings[key] == nil then
     return default
@@ -63,100 +10,87 @@ local function metaguiSettings(key, default)
 end
 
 local function buildList()
-  widget.clearListItems("scroll.list") -- clear out first
-  local c = root.assetJson("/quickbar/icons.json")
+  widget.clearListItems("scroll.list")
+
+  local iconConfig = root.assetJson("/quickbar/icons.json")
   local items = { }
-  
-  -- stardust settings
-  local settings = c.items["metagui:settings"]
-  if settings then settings.label = "^pat_stardust;Stardust "..settings.label end
-  
-  -- stardust qb icon
-  local starqb = config.getParameter("stardust_qb_icon")
-  if starqb then c.items["__stardustquickbar"] = starqb end
-    
+
   -- translate legacy entries
-  for _,i in ipairs(c.priority) do
-    c.items["_legacy.priority:"..i.label] = {
-      label = "^essential;"..i.label,
-      icon = i.icon,
-      weight = -1100,
-      action = legacyAction(i)
-    }
-  end
-  if player.isAdmin() then
-    for _,i in ipairs(c.admin) do
-      c.items["_legacy.admin:"..i.label] = {
-        label = "^admin;"..i.label,
-        icon = i.icon, weight = -1000,
-        action = legacyAction(i),
-        condition = { "admin" }
+  for k, tr in pairs(iconConfig.legacyTranslation) do
+    for _, item in ipairs(iconConfig[k]) do
+      local id = string.format("_legacy.%s:%s", k, item.label)
+      iconConfig.items[id] = {
+        label = (tr.prefix or "")..item.label,
+        icon = item.icon..(tr.directives or ""),
+        weight = tr.weight or 0,
+        action = legacyAction(item)
       }
     end
   end
-  for _,i in ipairs(c.normal) do
-    c.items["_legacy.normal:"..i.label] = {
-      label = i.label,
-      icon = i.icon,
-      action = legacyAction(i)
-    }
-  end
   
-  -- dump in items and sort them
-  local hidden = metaguiSettings("pat_hiddenIcons", {})
-  for k, i in pairs(c.items) do
-    if not hidden[k] and (not i.condition or condition(table.unpack(i.condition))) then
-      i._sort = string.lower(string.gsub(i.label, "(%b^;)", ""))
-      i.label = string.gsub(i.label, "(%b^;)", colorSub)
-      i.weight = i.weight or 0
-      table.insert(items, i)
+  local hiddenItems = getMetaguiSetting("pat_hiddenIcons", {})
+  
+  for k, item in pairs(iconConfig.items) do
+    if not hiddenItems[k] and (not item.condition or condition(table.unpack(item.condition))) then
+      local label = item.classicLabel or item.label
+      item._sort = string.gsub(label, "(%b^;)", ""):lower()
+      item.label = string.gsub(label, "(%b^;)", iconConfig.colorTags)
+      item.weight = item.weight or 0
+      table.insert(items, item)
     end
   end
+  
   table.sort(items, function(a, b) return a.weight < b.weight or (a.weight == b.weight and a._sort < b._sort) end)
   
-  -- and add items to pane list
-  for idx = 1, #items do
-    local i = items[idx]
-    local l = "scroll.list." .. widget.addListItem("scroll.list")
-    widget.setText(l .. ".label", i.label)
-    local bc = l .. ".buttonContainer"
-    widget.registerMemberCallback(bc, "click", function()
-      if i.condition and not condition(table.unpack(i.condition)) then return nil end -- recheck condition on attempt
-      action(table.unpack(i.action))
-
-      if (metaguiSettings("quickbarAutoDismiss", false) and not i.blockAutoDismiss) or i.dismissQuickbar then pane.dismiss() end
-    end)
-    local btn = bc .. "." .. widget.addListItem(bc) .. ".button"
-    widget.setButtonOverlayImage(btn, i.icon or "/items/currency/essence.png")
-    
-		compactLabels[btn] = i.label
+  for _, item in ipairs(items) do
+    addQuickbarItem(item)
   end
 end
 
-function init()
-  local m = getmetatable''
+local function itemCallback(item)
+  if item.condition and not condition(table.unpack(item.condition)) then -- recheck condition on attempt
+    pane.playSound("/sfx/interface/clickon_error.ogg")
+    return nil
+  end 
+
+  action(table.unpack(item.action))
+
+  if (not item.blockAutoDismiss and getMetaguiSetting("quickbarAutoDismiss", false)) or item.dismissQuickbar then
+    pane.dismiss()
+  end
+end
+
+function addQuickbarItem(item)
+  local newListItem = "scroll.list." .. widget.addListItem("scroll.list")
+  widget.setText(newListItem .. ".label", item.label)
+
+  local container = newListItem .. ".buttonContainer"
+  widget.registerMemberCallback(container, "click", function() itemCallback(item) end)
   
+  local button = container .. "." .. widget.addListItem(container) .. ".button"
+  widget.setButtonOverlayImage(button, item.icon or "/items/currency/essence.png")
+end
+
+function init()
+  local shared = getmetatable''
+
   -- close stardust quickbar
-  local ipc = m.metagui_ipc
-  if ipc and ipc.uniqueByPath then
+  local ipc = shared.metagui_ipc
+  if ipc and ipc.uniqueByPath and not config.getParameter("mgipc_noDismiss") then
     local path = root.assetJson("/metagui/registry.json:panes.quickbar.quickbar")
     if ipc.uniqueByPath[path] then
       ipc.uniqueByPath[path]()
-      
-      if not config.getParameter("mgipc_noDismiss") then
-        pane.dismiss()
-        return
-      end
+      return pane.dismiss()
     end
   end
   
-  if m.pat_classicqb_dismiss then
-    m.pat_classicqb_dismiss()
+  if shared.pat_classicqb_dismiss then
+    shared.pat_classicqb_dismiss()
     return pane.dismiss()
   end
   
-  m.pat_classicqb_dismiss = pane.dismiss
-  m.pat_classicqb_rebuild = buildList
+  shared.pat_classicqb_dismiss = pane.dismiss
+  shared.pat_classicqb_rebuild = buildList
   
   buildList()
 end
@@ -164,7 +98,7 @@ end
 function uninit()
   widget.clearListItems("scroll.list")
   
-  local m = getmetatable''
-  m.pat_classicqb_dismiss = nil
-  m.pat_classicqb_rebuild = nil
+  local shared = getmetatable''
+  shared.pat_classicqb_dismiss = nil
+  shared.pat_classicqb_rebuild = nil
 end
